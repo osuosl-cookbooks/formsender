1490ac48437c0461182df91ceab8bcb32edc305f
matthewrsj <mrsj@osuosl.com>
2015-08-06 14:41:43 -0700

    Initial Commit of Formsender

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+formsender CHANGELOG
+====================
+This file is used to list changes made in each version of the
+formsender cookbook.
+
+0.1.0
+-----
+- Initial release of formsender
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'formsender'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'apache2'
+description      'Installs/Configures formsender'
+long_description 'Installs/Configures formsender'
+version          '0.1.0'
+
