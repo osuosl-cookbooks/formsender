98250f0a59710f3fe5991bc1211351481a0a0cea
matthewrsj <mrsj@osuosl.com>
2015-08-17 16:52:10 -0700

    Updated git branch to 'develop' for staging

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['formsender']['venv_owner'] = 'root'
 default['formsender']['venv_group'] = 'root'
 
 default['formsender']['debug'] = false
-default['formsender']['git_branch'] = '91eb2422806d44f819d85cbf2055bdea4c707df4'
+default['formsender']['git_branch'] = 'develop'
 default['formsender']['repository'] = 'https://github.com/osuosl/formsender'
 
 default['formsender']['server_name'] = node['fqdn']
