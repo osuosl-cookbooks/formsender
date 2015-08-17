b27942ba6a149c789506e632331ab52ad37b0155
matthewrsj <mrsj@osuosl.com>
2015-08-17 11:39:05 -0700

    Fixed foodcritic and rubocop errors

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,2 +1,2 @@
-node.override['python']['pip_location'] = "#{node['python']['prefix_dir']}" \
-'/bin/pip2.7'
+node.override['python']['pip_location'] = \
+"#{node['python']['prefix_dir']}/bin/pip2.7"
